49e7078314d112c5a7bdcead327bf896fea45295
erickguimaraes <erickgdeoliveira@gmail.com>
2019-06-29 14:08:10 -0300

    adding junction to offense code

--- machineLearnig.py
+++ machineLearnig.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from IPython.display import display
 from tqdm import tqdm, tqdm_pandas
+
 def main():
     print("Loading Data...")
     names = ["INCIDENT_ID", "OFFENSE_ID", "OFFENSE_CODE", "OFFENSE_CODE_EXTENSION",
@@ -21,12 +22,17 @@ def main():
     #display(data.groupby([data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
     temp=display(data.groupby([data.INCIDENT_ID,data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
     print(temp)
+
+    data['OFENSE_CODE_JUNCTION'] = data[['OFFENSE_CODE','OFFENSE_CODE_EXTENSION']].dot([100,1])
+    print("data junction he ==========================")
+    print(data['OFENSE_CODE_JUNCTION'])
     #filter out traffic accidents from the crime dataset
     data=data[data['IS_CRIME']==1]
     data['HOUR_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).hour
     data['WEEKDAY_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).weekday
     data['MONTH_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).month
     data['YEAR_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).year
+
     #data['OFFENSE_TYPE_CODE']= pd.Int64Index((data['OFFENSE_CODE'].astype(str)+data['OFFENSE_CODE_EXTENSION'].astype(str)).astype(64))
     #pd.set_option('display.max_rows',500)
     print(data)
