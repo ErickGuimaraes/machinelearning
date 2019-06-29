47d921869e7189dae1effa6a1bd6fac8c6c52970
erickguimaraes <erickgdeoliveira@gmail.com>
2019-06-28 21:41:45 -0300

    improving pandas exeperience

--- machineLearnig.py
+++ machineLearnig.py
@@ -1,7 +1,27 @@
 import pandas as pd
 
-data = pd.read_csv("db/crime.csv")
+import matplotlib.pyplot as plt
+from Tools.scripts.dutree import display
+from scipy import stats
+import numpy as np
 
-print(data.head)
+
+names = ["INCIDENT_ID", "OFFENSE_ID", "OFFENSE_CODE", "OFFENSE_CODE_EXTENSION",
+         "OFFENSE_TYPE_ID", "OFFENSE_CATEGORY_ID", "FIRST_OCCURRENCE_DATE",
+         "LAST_OCCURRENCE_DATE", "REPORTED_DATE", "INCIDENT_ADDRESS",
+         "GEO_X", "GEO_Y", "GEO_LON", "GEO_LAT", "DISTRICT_ID", "PRECINCT_ID",
+         "NEIGHBORHOOD_ID", "IS_CRIME", "IS_TRAFFIC"]
+
+#data = pd.read_csv("db/crime.csv",sep= ",",)
+data=pd.read_csv('db/crime.csv', parse_dates=True)
+
+data.info()
+
+print(data)
+
+temp=display(data.groupby([data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
+pd.set_option('display.max_rows',500)
+print(temp)
+
 
 
