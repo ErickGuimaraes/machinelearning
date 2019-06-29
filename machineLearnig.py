c9697bc16813ba2c3f3491816e647656a407b23d
jotave42 <jvictorcostamelo@gmail.com>
2019-06-29 13:20:43 -0300

    created coluns on tables

--- machineLearnig.py
+++ machineLearnig.py
@@ -6,23 +6,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 from IPython.display import display
+from tqdm import tqdm, tqdm_pandas
 def main():
-    print("carregando dados...")
+    print("Loading Data...")
     names = ["INCIDENT_ID", "OFFENSE_ID", "OFFENSE_CODE", "OFFENSE_CODE_EXTENSION",
             "OFFENSE_TYPE_ID", "OFFENSE_CATEGORY_ID", "FIRST_OCCURRENCE_DATE",
-            "LAST_OCCURRENCE_DATE", "REPORTED_DATE", "INCIDENT_ADDRESS",
-            "GEO_X", "GEO_Y", "GEO_LON", "GEO_LAT", "DISTRICT_ID", "PRECINCT_ID",
+            "REPORTED_DATE", "DISTRICT_ID", "PRECINCT_ID",
             "NEIGHBORHOOD_ID", "IS_CRIME", "IS_TRAFFIC"]
-
-#data = pd.read_csv("db/crime.csv",sep= ",",)
     data=pd.read_csv('db/crime.csv', parse_dates=True)
-
+    print("===================================DATA INFO======================================")
     data.info()
-
+    print("===================================DATA INFO======================================")
+    print(data.head(5))
+    #display(data.groupby([data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
+    temp=display(data.groupby([data.INCIDENT_ID,data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
+    print(temp)
+    #filter out traffic accidents from the crime dataset
+    data=data[data['IS_CRIME']==1]
+    data['HOUR_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).hour
+    data['WEEKDAY_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).weekday
+    data['MONTH_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).month
+    data['YEAR_REPORTED']=pd.DatetimeIndex(data['REPORTED_DATE']).year
+    #data['OFFENSE_TYPE_CODE']= pd.Int64Index((data['OFFENSE_CODE'].astype(str)+data['OFFENSE_CODE_EXTENSION'].astype(str)).astype(64))
+    #pd.set_option('display.max_rows',500)
     print(data)
-
-    display(data.groupby([data.OFFENSE_CODE,data.OFFENSE_CODE_EXTENSION,data.OFFENSE_TYPE_ID]).size())
-
-
 
 main()
